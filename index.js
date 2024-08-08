31b434a230c3a604a58ba6c6efcdeabbc9394035
Jonathan Samuels <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2024-08-08 15:06:27 -0700

    Edge case handling

--- index.js
+++ index.js
@@ -33,12 +33,19 @@ app.post("/register", async (req, res) => {
   const email = req.body.username;
   const password = req.body.password;
   try{
-    const result = await db.query(
+    const checkResult = await db.query(
     "INSERT INTO users (email, password) VALUES ($1, $2)", 
-    [email, password]);
+    [email,]);
+
+    if(checkResult.rows.length > 0){
+      res.send("Email already exists. Try logging in.");
+    } else {
+      const result = await db.query(
+        "INSERT INTO users (email, password VALUES ($1, $2)", [email, password]
+      );
+    }
     console.log(result);
     res.render("secrets.ejs");
-  
   } catch(err){
     console.log(err);
   }
