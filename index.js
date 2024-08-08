3f1928208d85100d2d38bd99cb028393b493430e
Jonathan Samuels <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2024-08-08 13:58:25 -0700

    addition of async db query & piline

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
-
+import pg from "pg"
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "secrets",
+  password: "Password*",
+});
+
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -25,18 +34,20 @@ app.get("/register", (req, res) => {
 // These are the two methods you need to define bruh.
 // Dont confuse get methods with post methods let this be your lesson
 app.post("/register", async (req, res) => {
+  const email = req.body.username;
   const password = req.body.password;
-  const username = req.body.username;
-  console.log(username);
-  console.log(password);
+  // Use the pg query to create a new user and then insert them into the database
+  try{
+    await db.query("INSERT INTO secrets (username, password) VALUES ($1, $2)", [email, password]);
+  } catch(err){
+    console.log(err);
+  }
   res.render("secrets.ejs");
 });
 
 app.post("/login", async (req, res) => {
+  const  email = req.body.username;
   const password = req.body.password;
-  const  username = req.body.username;
-  console.log(username);
-  console.log(password);
   res.render("secrets.ejs");
 });
 
